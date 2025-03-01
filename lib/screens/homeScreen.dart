22eb45405cc5688161e70a8fc485869932f3206a
Mario Florez <mario.florez249@gmail.com>
2025-02-28 23:20:50 -0500

    Se agregan las cards a la pantalla principal con botones de acceso a las funcionalidades principales, se adaptan los botones para que realicen el ruteo a la pantalla correspondiente.

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -11,9 +11,9 @@ class _HomeScreenState extends State<HomeScreen> {
   List<bool> _isExpanded = [false, false, false];
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sistema de Gestión de Nómina"),),
+      appBar: AppBar(title: Text("Sistema de Gestión de Nómina")),
       drawer: Drawer(
         child: ListView(
           children: [
@@ -27,41 +27,111 @@ class _HomeScreenState extends State<HomeScreen> {
                 _buildExpansionPanel(
                   index: 0,
                   title: "Empleados",
-                  options: ["Crear empleado", "Ver empleados"]
+                  options: {"Crear empleado": "create-employee", "Ver empleados": "retrieve-employees"},
                 ),
                 _buildExpansionPanel(
                   index: 1,
                   title: "Novedades",
-                  options: ["Crear novedad", "Ver novedades"]
+                  options: {"Crear novedad": "create-new", "Ver novedades": "retrieve-news"},
                 ),
                 _buildExpansionPanel(
                   index: 2,
                   title: "Nómina",
-                  options: ["Crear nómina", "Ver nóminas"]
+                  options: {"Crear nómina": "create-payroll", "Ver nóminas": "retrieve-payrolls"},
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
-      body: Center(child: Text("Contenido principal")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildCard(
+              "Empleados",
+              "create-employee",
+              "retrieve-employees",
+              context,
+            ),
+            _buildCard("Novedades", "create-new", "retrieve-news", context),
+            _buildCard(
+              "Nóminas",
+              "create-payroll",
+              "retrieve-payrolls",
+              context,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  ExpansionPanel _buildExpansionPanel({required int index, required String title, required List<String> options}){
+  ExpansionPanel _buildExpansionPanel({
+    required int index,
+    required String title,
+    required Map<String,String> options,
+  }) {
     return ExpansionPanel(
-      headerBuilder: (context, isExpanded){
-        return ListTile(title: Text(title),);
+      headerBuilder: (context, isExpanded) {
+        return ListTile(title: Text(title));
       },
       body: Column(
-        children: options.map((option) {
-          return ListTile(
-            title: Text(option),
-            // onTap,
-          );
-        }).toList(),
+        children:
+            options.entries.map((option) {
+              return ListTile(
+                title: Text(option.key),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/${option.value}');
+                },
+              );
+            }).toList(),
       ),
-    isExpanded: _isExpanded[index],
+      isExpanded: _isExpanded[index],
+    );
+  }
+
+  Widget _buildCard(
+    String title,
+    String createPath,
+    String retrievePath,
+    BuildContext context,
+  ) {
+    return SizedBox(
+      height: 208,
+      width: 300,
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 12),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => Navigator.pushNamed(context, '/$createPath'),
+                  child: Text('Crear $title'),
+                ),
+                SizedBox(height: 12),
+                ElevatedButton(
+                  onPressed:
+                      () => Navigator.pushNamed(context, '/$retrievePath'),
+                  child: Text('Consultar $title'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
